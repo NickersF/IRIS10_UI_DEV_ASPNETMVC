5201ec56cb9791a733188d9a046d28e394daff58
Nicholas Fazzolari <nickfazzpdx@gmail.com>
2020-12-11 13:42:24 -0800

    Initial from local. Project created.

---
+++ IRIS10_UI_Dev_ASPNETMVC5/IRIS10_UI_Dev_ASPNETMVC5/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace IRIS10_UI_Dev_ASPNETMVC5
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ IRIS10_UI_Dev_ASPNETMVC5/IRIS10_UI_Dev_ASPNETMVC5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IRIS10_UI_Dev_ASPNETMVC5.MvcApplication" Language="C#" %>
