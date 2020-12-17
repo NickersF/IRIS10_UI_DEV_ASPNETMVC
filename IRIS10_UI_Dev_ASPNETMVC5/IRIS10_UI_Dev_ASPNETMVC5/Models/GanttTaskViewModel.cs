d7aaafc2099abf0f758d11dc004a59a022182f97
Nicholas Fazzolari <nickfazzpdx@gmail.com>
2020-12-17 14:46:48 -0800

    Implemented gantt example instance. Currently no data coming through.

---
+++ IRIS10_UI_Dev_ASPNETMVC5/IRIS10_UI_Dev_ASPNETMVC5/Models/GanttTaskViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Kendo.Mvc.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IRIS10_UI_Dev_ASPNETMVC5.Models
+{
+	public class TaskViewModel : IGanttTask
+	{
+		public int TaskID { get; set; }
+
+		public int? ParentID { get; set; }
+
+		public int OrderId { get; set; }
+
+        public int OrderID { get; set; }
+        public bool Expanded { get; set; }
+
+		public string Title { get; set; }
+
+		public DateTime Start { get; set; }
+
+		public DateTime End { get; set; }
+
+		public decimal PercentComplete { get; set; }
+
+		public bool Summary { get; set; }
+
+		public DateTime PlannedStart { get; set; }
+
+		public DateTime PlannedEnd { get; set; }
+
+        public int ID { get; set; }
+    }
+}
---
+++ IRIS10_UI_Dev_ASPNETMVC5/IRIS10_UI_Dev_ASPNETMVC5/Models/SampleEntities.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IRIS10_UI_Dev_ASPNETMVC5.Models
+{
+    public class SampleEntities
+    {
+        public IList<TaskViewModel> GanttTasks { get; set; }
+
+        public IList<DependencyViewModel> GanttDependencies { get; set; }
+    }
+}
