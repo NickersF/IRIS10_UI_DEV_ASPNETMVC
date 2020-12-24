c2ad3c583882e796b7318fc2d8b55e31f6b58478
Nicholas Fazzolari <nickfazzpdx@gmail.com>
2020-12-24 13:35:43 -0800

    Implemented pop up windows

---
+++ IRIS10_UI_Dev_ASPNETMVC5/IRIS10_UI_Dev_ASPNETMVC5/Scripts/custom/ReportUpper.js
@@ -0,0 +1,36 @@
+﻿// Manages UI state and events for the upper report page
+
+$(document).ready(function () {
+
+    // Basic configuration of the window instance
+    var WindowOptions = {
+        actions: ["Maximize", "Close"],
+        modal: true,
+        draggable: false,
+        width: "512px",
+        height: "256px",
+        visible: false
+    }
+
+    $("#SupportWindow").kendoWindow(WindowOptions);         // Set the options and initialize support window
+    $("#UserWindow").kendoWindow(WindowOptions);            // Set the options and initialize user window
+    $("#CustomReportWindow").kendoWindow(WindowOptions);    // Set the options and initialize custom reports window
+
+    // Drives down into the overflow buttons of the ToolBar items and sets events
+    $.ajax({
+        global: false,
+        type: "GET",
+        url: "/ReportScreens/ReportUpper",
+        data: {}
+    }).done(() => {
+        $("#SupportBtn_overflow").click( () => {
+            $("#SupportWindow").data("kendoWindow").center().open();
+        });
+        $("#UserBtn_overflow").click( () => {
+            $("#UserWindow").data("kendoWindow").center().open();
+        });
+        $("#CustomReportBtn").click(() => {
+            $("#CustomReportWindow").data("kendoWindow").center().open();
+        });
+    });
+});
---
+++ IRIS10_UI_Dev_ASPNETMVC5/IRIS10_UI_Dev_ASPNETMVC5/Scripts/custom/windowEx_openJQWindow.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+
+    // Basic configuration of the window instance
+    var WindowOptions = {
+        actions: ["Maximize", "Close"],
+        modal: true,
+        draggable: false,
+        width: "512px",
+        height: "256px",
+        visible: false
+    }
+
+    $("#KendoWindowJQuery").kendoWindow(WindowOptions); // Set the options
+
+    // Open the window via the UI button element
+    $("#openJsKendoWindowBtn").click(() => {
+        $("#KendoWindowJQuery").data("kendoWindow").center().open();
+    });
+
+});
