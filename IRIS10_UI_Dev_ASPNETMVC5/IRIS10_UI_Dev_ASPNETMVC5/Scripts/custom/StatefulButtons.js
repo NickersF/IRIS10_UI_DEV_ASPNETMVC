d9e1a98b527ad1d82a96685462c2541367e047eb
Nicholas Fazzolari <nickfazzpdx@gmail.com>
2021-02-12 15:13:56 -0800

    Added some button examples.

---
+++ IRIS10_UI_Dev_ASPNETMVC5/IRIS10_UI_Dev_ASPNETMVC5/Scripts/custom/StatefulButtons.js
@@ -0,0 +1,30 @@
+﻿$(document).ready(function () {
+    var condVal = 1;
+
+    $("#ToggleButtonState").click(function () {
+
+        console.log("FUCK YOU!" + condVal);
+        //if (condVal == 1) {
+        //    $("#StateFullButton_BS").removeClass("btn-primary");
+        //    $("#StateFullButton_BS").addClass("btn-danger");
+
+        //    console.log();
+
+        //    condVal = 0;
+        //}
+
+        //if (condVal == 0) {
+        //    $("#StateFullButton_BS").removeClass("btn-danger");
+        //    $("#StateFullButton_BS").addClass("btn-primary");
+
+        //    console.log($("#StateFullButton_BS"));
+
+        //    condVal = 1;
+        //}
+
+
+        $("#StateFullButton_BS").removeClass("btn-primary");
+        $("#StateFullButton_BS").addClass("btn-danger");
+    });
+
+}); 
