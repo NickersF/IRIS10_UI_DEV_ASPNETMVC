6140864dab61000e1833a8f80b8eb1080463d617
Nicholas Fazzolari <nickfazzpdx@gmail.com>
2021-02-19 15:28:59 -0800

    Added Permit new permit custom input form examples.

---
+++ IRIS10_UI_Dev_ASPNETMVC5/IRIS10_UI_Dev_ASPNETMVC5/Controllers/PermitsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IRIS10_UI_Dev_ASPNETMVC5.Controllers
+{
+    public class PermitsController : Controller
+    {
+        // GET: AddStandardPermit
+        public ActionResult AddStandardPermit()
+        {
+            ViewBag.Title = "Add Standard Permit Form Grid";
+
+            return View();
+        }
+
+        // GET: CommonFormInputs
+        public ActionResult CommonFormInputs()
+        {
+            ViewBag.Title = "Common Form Input Elements (Bootstrap and Kendo UI).";
+
+            return View();
+        }
+    }
+}
