cb51d0f3e78ae1189706da6aad713d67ea86e937
Nicholas Fazzolari <nickfazzpdx@gmail.com>
2021-03-11 15:30:14 -0800

    Added ODOT UI login pages.

---
+++ IRIS10_UI_Dev_ASPNETMVC5/IRIS10_UI_Dev_ASPNETMVC5/Controllers/ODOTMilageController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IRIS10_UI_Dev_ASPNETMVC5.Controllers
+{
+    public class ODOTMilageController : Controller
+    {
+        public ActionResult LoginSelect()
+        {
+            return View();
+        }
+
+        // GET: ODOTMilage
+        public ActionResult ODOTLogin()
+        {
+            return View();
+        }
+
+        public ActionResult AOCLogin()
+        {
+            return View();
+        }
+    }
+}
---
+++ IRIS10_UI_Dev_ASPNETMVC5/IRIS10_UI_Dev_ASPNETMVC5/Scripts/custom/ODOT_UI.js
@@ -0,0 +1,32 @@
+﻿// Author: Nicholas Fazzolari
+// JS for the ODOT UI events and state
+
+$(document).ready(function () {
+
+    // Set initial state
+    $("#loginButtonsWrapper").fadeIn(900).addClass("d-flex");
+
+    $("#loginSelectODOT_btn").click(function () {
+        $("#loginButtonsWrapper").hide().removeClass("d-flex");
+        $("#loginFormODOTWrapper").fadeIn(900).addClass("d-flex");
+    });
+
+    $("#loginSelectCounties_btn").click(function () {
+        $("#loginButtonsWrapper").hide().removeClass("d-flex");
+        $("#loginFormCountiesWrapper").fadeIn(900).addClass("d-flex");
+    });
+
+    $("#loginSelectHeader").click(function () {
+        $("#loginButtonsWrapper").fadeIn(900).addClass("d-flex");
+        $("#loginFormODOTWrapper").hide().removeClass("d-flex");
+        $("#loginFormCountiesWrapper").hide().removeClass("d-flex");
+    });
+
+    if ($("#loginButtonsWrapper").css("display") == "none") {
+        console.log("Element not visible");
+    } else {
+        console.log("Element already visible.");
+    }
+
+    console.log("ODOT_UI.js Loaded");
+});
