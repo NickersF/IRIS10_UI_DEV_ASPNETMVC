86df91cddd1f53e1a04e077ff5dc19a766afb784
Nicholas Fazzolari <nickfazzpdx@gmail.com>
2021-05-04 15:29:09 -0700

    Added the basics of the step wizard.

---
+++ IRIS10_UI_Dev_ASPNETMVC5/IRIS10_UI_Dev_ASPNETMVC5/Controllers/StepWizardController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IRIS10_UI_Dev_ASPNETMVC5.Controllers
+{
+    public class StepWizardController : Controller
+    {
+        // GET: StepWizard
+        public ActionResult Index()
+        {
+            ViewBag.Title = "IRIS10 UI Development Sandbox - Step Wizard";
+            return View();
+        }
+    }
+}
