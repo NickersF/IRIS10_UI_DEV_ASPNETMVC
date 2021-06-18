2ce8d2bb238a0d37178c1ac3b69ad7d380f3838e
Nicholas Fazzolari <nickfazzpdx@gmail.com>
2021-06-18 15:48:28 -0700

    Added the user account menu

---
+++ IRIS10_UI_Dev_ASPNETMVC5/IRIS10_UI_Dev_ASPNETMVC5/Controllers/TemplatesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IRIS10_UI_Dev_ASPNETMVC5.Controllers
+{
+    public class TemplatesController : Controller
+    {
+        // GET: Templates
+        public ActionResult UserAccountMenuTemplate()
+        {
+            return PartialView("~/Views/Shared/EditorTemplates/UserAccountMenuTemplate.cshtml");
+        }
+    }
+}
